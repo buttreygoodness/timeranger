0b3b87fe069ee16a899c69b64e6a8d7cebc2afe5
buttreygoodness <jeff@readypress.com>
2016-01-22 13:49:41 -0700

    Making changes to a no-dblckick situation.

---
+++ src/indexbuster_no_dblclk.coffee
@@ -0,0 +1,123 @@
+#! timerange.js
+#! version : 1.1.0
+#! authors : Buster, Inc.
+#! license : GPL2
+
+###*
+ * Class for determining time ranges and applying corresponding changes to the dom.
+ * @param {Object} config configuration file containing directives.
+###
+class BusterTimeSeries
+  constructor: (@config) ->
+  
+  init: () ->
+    @targetDate = @targetDate || moment(@config.targetDate)
+    @currentDate = @currentDate || moment()
+    @evergreen_day_before = @config.evergreen_day_before || false
+    @evergreen_day_of = @config.evergreen_day_of || false
+    @getTimeDifference()
+
+  update: (target_date, current_date) ->
+    @targetDate = moment(target_date) || @targetDate
+    @currentDate = moment(current_date) || @currentDate
+    @getTimeDifference()
+  
+  setImageElement: (image_uri) ->
+
+    if !@config.imageElement
+      return
+
+    el = document.getElementById @config.imageElement
+    el.setAttribute 'src', image_uri
+    el.setAttribute 'source', image_uri
+
+  setVideoElement: (video_uri) ->
+
+    if !@config.videoElement
+      return
+    
+    el = document.getElementById @config.videoElement
+
+    if window.gwd
+      # el.gwdDeactivate()
+      el.setAttribute 'sources', video_uri
+
+      setTimeout () ->
+        el.src = el.childNodes[0].src
+        el.load()
+        # el.gwdActivate()
+      , 3000
+    else
+      el.setAttribute 'src', video_uri
+
+  getTimeDifference: () ->
+    week_before = @currentDate.isBefore(@targetDate, 'week')
+    this_week = @currentDate.isSame(@targetDate, 'week')
+    week_after = @currentDate.isAfter(@targetDate, 'week')
+
+    if week_before
+      return @setWeekBeforeImage()
+    if this_week
+      return @setThisWeekImage()
+    if week_after
+      return @setWeekAfterImage()
+
+  setWeekBeforeImage: ->
+    days_before = @currentDate.diff(@targetDate, 'days')
+    weeks_before = @currentDate.diff(@targetDate, 'weeks')
+    if weeks_before == -0 && days_before == -6
+      if @config.images
+        @setImageElement @config.images.outside_weeks_before
+      if @config.videos
+        @setVideoElement @config.videos.outside_weeks_before
+      return
+    if @config.images
+      @setImageElement @config.images[weeks_before + '_weeks'] || @config.images.outside_weeks_before
+    if @config.videos
+      @setVideoElement @config.videos[weeks_before + '_weeks'] || @config.videos.outside_weeks_before
+
+  setThisWeekImage: ->
+    days_before = @currentDate.diff(@targetDate, 'days')
+    temp_target_date = moment(@config.targetDate)
+    if @currentDate.isSame(@targetDate, 'day')
+      if @config.images
+        @setImageElement @config.images.tonight
+      if @config.videos
+        @setVideoElement @config.videos.tonight
+      return
+    if @currentDate.isAfter(@targetDate, 'day')
+      if @currentDate.isoWeekday() == @targetDate.isoWeekday()
+        if @config.images
+          @setImageElement @config.images.tonight
+        if @config.videos
+          @setVideoElement @config.videos.tonight
+        return
+      else
+        return @setWeekAfterImage(false)
+
+    if @config.images
+      @setImageElement @config.images[days_before + '_days'] || @config.images.outside_days_before
+    if @config.videos
+      @setVideoElement @config.videos[days_before + '_days'] || @config.videos.outside_days_before || @config.videos.outside_weeks_before
+
+  setWeekAfterImage: (evergreen) ->
+    weeks_after = @currentDate.diff(@targetDate, 'weeks')
+    if evergreen != false
+      if @currentDate.isoWeekday() == @targetDate.isoWeekday() && @evergreen_day_of
+        if @config.videos
+          @setVideoElement @config.videos.evergreen_day_of
+        if @config.images
+          @setImageElement @config.images.evergreen_day_of
+        return 
+      if @currentDate.isoWeekday() == @targetDate.isoWeekday() - 1 && @evergreen_day_before
+        if @config.videos
+          @setVideoElement @config.videos.evergreen_day_before
+        if @config.images
+          @setImageElement @config.images.evergreen_day_before
+        return
+    if @config.videos
+      @setVideoElement @config.videos[weeks_after + '_weeks_after'] || @config.videos.evergreen_weeks_after
+    if @config.images
+      @setImageElement @config.images[weeks_after + '_weeks_after'] || @config.images.evergreen_weeks_after
+
+window.BusterTimeSeries = BusterTimeSeries
---
+++ src/indexbuster_no_dblclk.js
@@ -0,0 +1,160 @@
+
+/**
+ * Class for determining time ranges and applying corresponding changes to the dom.
+ * @param {Object} config configuration file containing directives.
+ */
+var BusterTimeSeries;
+
+BusterTimeSeries = (function() {
+  function BusterTimeSeries(config) {
+    this.config = config;
+  }
+
+  BusterTimeSeries.prototype.init = function() {
+    this.targetDate = this.targetDate || moment(this.config.targetDate);
+    this.currentDate = this.currentDate || moment();
+    this.evergreen_day_before = this.config.evergreen_day_before || false;
+    this.evergreen_day_of = this.config.evergreen_day_of || false;
+    return this.getTimeDifference();
+  };
+
+  BusterTimeSeries.prototype.update = function(target_date, current_date) {
+    this.targetDate = moment(target_date) || this.targetDate;
+    this.currentDate = moment(current_date) || this.currentDate;
+    return this.getTimeDifference();
+  };
+
+  BusterTimeSeries.prototype.setImageElement = function(image_uri) {
+    var el;
+    if (!this.config.imageElement) {
+      return;
+    }
+    el = document.getElementById(this.config.imageElement);
+    el.setAttribute('src', image_uri);
+    return el.setAttribute('source', image_uri);
+  };
+
+  BusterTimeSeries.prototype.setVideoElement = function(video_uri) {
+    var el;
+    if (!this.config.videoElement) {
+      return;
+    }
+    el = document.getElementById(this.config.videoElement);
+    if (window.gwd) {
+      el.setAttribute('sources', video_uri);
+      return setTimeout(function() {
+        el.src = el.childNodes[0].src;
+        return el.load();
+      }, 3000);
+    } else {
+      return el.setAttribute('src', video_uri);
+    }
+  };
+
+  BusterTimeSeries.prototype.getTimeDifference = function() {
+    var this_week, week_after, week_before;
+    week_before = this.currentDate.isBefore(this.targetDate, 'week');
+    this_week = this.currentDate.isSame(this.targetDate, 'week');
+    week_after = this.currentDate.isAfter(this.targetDate, 'week');
+    if (week_before) {
+      return this.setWeekBeforeImage();
+    }
+    if (this_week) {
+      return this.setThisWeekImage();
+    }
+    if (week_after) {
+      return this.setWeekAfterImage();
+    }
+  };
+
+  BusterTimeSeries.prototype.setWeekBeforeImage = function() {
+    var days_before, weeks_before;
+    days_before = this.currentDate.diff(this.targetDate, 'days');
+    weeks_before = this.currentDate.diff(this.targetDate, 'weeks');
+    if (weeks_before === -0 && days_before === -6) {
+      if (this.config.images) {
+        this.setImageElement(this.config.images.outside_weeks_before);
+      }
+      if (this.config.videos) {
+        this.setVideoElement(this.config.videos.outside_weeks_before);
+      }
+      return;
+    }
+    if (this.config.images) {
+      this.setImageElement(this.config.images[weeks_before + '_weeks'] || this.config.images.outside_weeks_before);
+    }
+    if (this.config.videos) {
+      return this.setVideoElement(this.config.videos[weeks_before + '_weeks'] || this.config.videos.outside_weeks_before);
+    }
+  };
+
+  BusterTimeSeries.prototype.setThisWeekImage = function() {
+    var days_before, temp_target_date;
+    days_before = this.currentDate.diff(this.targetDate, 'days');
+    temp_target_date = moment(this.config.targetDate);
+    if (this.currentDate.isSame(this.targetDate, 'day')) {
+      if (this.config.images) {
+        this.setImageElement(this.config.images.tonight);
+      }
+      if (this.config.videos) {
+        this.setVideoElement(this.config.videos.tonight);
+      }
+      return;
+    }
+    if (this.currentDate.isAfter(this.targetDate, 'day')) {
+      if (this.currentDate.isoWeekday() === this.targetDate.isoWeekday()) {
+        if (this.config.images) {
+          this.setImageElement(this.config.images.tonight);
+        }
+        if (this.config.videos) {
+          this.setVideoElement(this.config.videos.tonight);
+        }
+        return;
+      } else {
+        return this.setWeekAfterImage(false);
+      }
+    }
+    if (this.config.images) {
+      this.setImageElement(this.config.images[days_before + '_days'] || this.config.images.outside_days_before);
+    }
+    if (this.config.videos) {
+      return this.setVideoElement(this.config.videos[days_before + '_days'] || this.config.videos.outside_days_before || this.config.videos.outside_weeks_before);
+    }
+  };
+
+  BusterTimeSeries.prototype.setWeekAfterImage = function(evergreen) {
+    var weeks_after;
+    weeks_after = this.currentDate.diff(this.targetDate, 'weeks');
+    if (evergreen !== false) {
+      if (this.currentDate.isoWeekday() === this.targetDate.isoWeekday() && this.evergreen_day_of) {
+        if (this.config.videos) {
+          this.setVideoElement(this.config.videos.evergreen_day_of);
+        }
+        if (this.config.images) {
+          this.setImageElement(this.config.images.evergreen_day_of);
+        }
+        return;
+      }
+      if (this.currentDate.isoWeekday() === this.targetDate.isoWeekday() - 1 && this.evergreen_day_before) {
+        if (this.config.videos) {
+          this.setVideoElement(this.config.videos.evergreen_day_before);
+        }
+        if (this.config.images) {
+          this.setImageElement(this.config.images.evergreen_day_before);
+        }
+        return;
+      }
+    }
+    if (this.config.videos) {
+      this.setVideoElement(this.config.videos[weeks_after + '_weeks_after'] || this.config.videos.evergreen_weeks_after);
+    }
+    if (this.config.images) {
+      return this.setImageElement(this.config.images[weeks_after + '_weeks_after'] || this.config.images.evergreen_weeks_after);
+    }
+  };
+
+  return BusterTimeSeries;
+
+})();
+
+window.BusterTimeSeries = BusterTimeSeries;
